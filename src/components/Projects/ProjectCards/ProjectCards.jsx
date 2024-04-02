1b08acf64542f81fe1c516b132803e3541b609b3
ValKozz <v.kozarev98@gmail.com>
2024-04-02 10:03:44 +0300

    Implemenent data.json to store projects and display them as cards

--- src/components/Projects/ProjectCards/ProjectCards.jsx
+++ src/components/Projects/ProjectCards/ProjectCards.jsx
@@ -1,18 +1,20 @@
 import "./projectCards.css";
 import Card from "./Card";
+import data from "./data.json";
 
 export default function ProjectCards() {
-  const content =
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis dolorem dicta consequatur! Nulla ab expedita provident possimus impedit iusto saepe!";
-
   return (
     <div className="card-container">
-      <Card title={"Cows and Bulls"} text={content} link="https://google.com" />
-      <Card title={"Test"} text={content} img_source={false} />
-      <Card title={"Test"} text={content} img_source={false} />
-      <Card title={"Test"} text={content} img_source={false} />
-      <Card title={"Test"} text={content} img_source={false} />
-      <Card title={"Test"} text={content} img_source={false} />
+      {data.map(function (project) {
+        return (
+          <Card
+            key={project.name}
+            title={project.name}
+            link={project.link}
+            text={project.desc}
+          />
+        );
+      })}
     </div>
   );
 }
